e91bdd8d54a5c9577fb7c384ad40dc6daedc9f59
heptaspirit <CAK115JA30cc>
2020-01-06 21:25:50 +0800

    地板生成

---
+++ Weekly130/Assets/Scripts/PlayGroundGenetator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGroundGenetator : MonoBehaviour
+{
+    public GameObject GroundPlanePrefab;    //普通地板
+    public GameObject ObstaclePrefab;       //障碍物
+    public int GroundLenght = 50;           //长度
+
+    void Start()
+    {
+        for( var x = 0; x < GroundLenght; x++)
+        {
+            for(var z = 0; z <GroundLenght; z++)
+            {
+                Instantiate(GetPlanePrefab(), new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+    private GameObject GetPlanePrefab()
+    {
+        int GroundChange = (int)Random.Range(0,10);
+        if (GroundChange > 8)
+        {
+            return ObstaclePrefab;
+        }
+        else
+        {
+            return GroundPlanePrefab;
+        }
+    }
+
+}
