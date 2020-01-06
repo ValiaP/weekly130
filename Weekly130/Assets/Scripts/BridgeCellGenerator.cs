56187b8f7450f9b7f9befefd10ea42cb2e616465
heptaspirit <CAK115JA30cc>
2020-01-06 23:01:04 +0800

    镜头以及地板效果调整

--- Weekly130/Assets/Scripts/BridgeCellGenerator.cs
+++ Weekly130/Assets/Scripts/BridgeCellGenerator.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class BridgeCellGenerator : MonoBehaviour
 {
 
+    public GameObject Ground;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.tag == "Player")
         {
-            if(this.CompareTag("NormalCell"))
+            Debug.Log("检测到进入");
+            if(this.gameObject.tag == "NormalCell")
             {
                 this.tag = "TrackCell";                                 //标记为拖尾块
+                Debug.Log("更改区块为拖尾块");
                 Material[] materials = new Material[]
                 {
                     Resources.Load("Res/Materials/TrackMapCell") as Material,
                 };
-                this.GetComponent<MeshRenderer>().materials = materials; //替换材质
+                Ground.GetComponent<MeshRenderer>().materials = materials; //替换材质
+                Debug.Log("更改区块地板材质");
             }
         }
     }
