d4365f8d6e6997256ccffa278c7ba3d31f854b04
heptaspirit <CAK115JA30cc>
2020-01-06 19:51:32 +0800

    尝试编写地图预制体控制脚本

---
+++ Weekly130/Assets/Scripts/BridgeCellGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeCellGenerator : MonoBehaviour
+{
+    public GameObject MyObj;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(MyObj.CompareTag("NormalCell"))
+            {
+                MyObj.tag = "TrackCell";
+            }
+        }
+    }
+
+}
