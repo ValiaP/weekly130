3b1a7b770aa4dcd713518bf40a417c12a37ac6cb
ll <ll@gmail.com>
2020-01-06 16:47:27 +0800

    #22

---
+++ Weekly130/Assets/GameEnd.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameEnd : MonoBehaviour
+{
+    public GameObject EndPanel;
+    public GameObject Win,Lose;
+
+    public Button Restart;
+
+
+    public void End(bool isWin)
+    {
+        EndPanel.SetActive(true);
+
+        if (isWin)
+        {
+            Win.SetActive(true);
+            Lose.SetActive(false);
+
+        }
+        else
+        {
+            Win.SetActive(false);
+            Lose.SetActive(true);
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Restart.onClick.AddListener(()=> {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
