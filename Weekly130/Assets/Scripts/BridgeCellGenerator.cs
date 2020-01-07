0fb6554ce756ad42dfef0f3f36a7fbb37273cca8
heptaspirit <CAK115JA30cc>
2020-01-07 10:39:38 +0800

    修改一些材质替换的问题

--- Weekly130/Assets/Scripts/BridgeCellGenerator.cs
+++ Weekly130/Assets/Scripts/BridgeCellGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class BridgeCellGenerator : MonoBehaviour
 {
@@ -10,17 +11,12 @@ public class BridgeCellGenerator : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            Debug.Log("检测到进入");
             if(this.gameObject.tag == "NormalCell")
             {
                 this.tag = "TrackCell";                                 //标记为拖尾块
                 Debug.Log("更改区块为拖尾块");
-                Material[] materials = new Material[]
-                {
-                    Resources.Load("Res/Materials/TrackMapCell") as Material,
-                };
-                Ground.GetComponent<MeshRenderer>().materials = materials; //替换材质
-                Debug.Log("更改区块地板材质");
+                Material mat = AssetDatabase.LoadAssetAtPath<Material>("Assets/Res/Materials/TrackMapCell.mat");
+                Ground.GetComponent<MeshRenderer>().sharedMaterial = mat;
             }
         }
     }
